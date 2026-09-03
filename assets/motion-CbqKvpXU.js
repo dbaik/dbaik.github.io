3f1189a25d6915c176b4baf4ffc7c337fcb5df22
dbaik <dbaik@users.noreply.github.com>
2026-09-03 18:16:34 +0200

    Publish rebuild to GitHub Pages.
    Copy the production build from portfolio/dist into the site root so Pages serves the merged hiring-page changes.

--- assets/motion-CbqKvpXU.js
+++ assets/motion-CbqKvpXU.js
@@ -1,4 +1,4 @@
-import{r as w}from"./icons-D03uf-8F.js";var me={exports:{}},Vt={};/**
+import{r as w}from"./icons-CYdflcfn.js";var me={exports:{}},Vt={};/**
  * @license React
  * react-jsx-runtime.production.js
  *
